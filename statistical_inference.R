68c7fd7ac28338bea623c87bc5adff47bbc60ca4
Marcohhh <marcotriacca@gmail.com>
2020-10-27 18:19:58 +0100

    New section in rmarkdown

--- statistical_inference.R
+++ statistical_inference.R
@@ -73,3 +73,58 @@ switch(n/10,hist(ctl, col="blue"), hist(ctl, col="red"), hist(ctl, col="green"),
 
 
 
+
+
+
+
+
+
+x <-  seq(50, 150,1)
+plot(x = x,
+     y = dnorm(x,100,15),
+     type = "l",                            # Specify that you want to plot a line graph
+     lwd = 2,                               # Thickness of line
+     lty = 1,
+     col = "black",                          # Colour of line
+     xlim=c(40,160),                          # Set limit of x-axis
+     frame.plot=TRUE,                       # Do plot the frame of the graph
+     xlab=" ",                    # Title for x-axis
+     ylab= " ",                             # Title for y-axis
+     axes = FALSE,                          # Don't plot the scales by default
+     main=paste(" "))     # Main title
+title(main="Area Under a Normal Distribution") # "line" adjusts distance to axis, cex.lab adjusts size
+title(xlab="Standard Deviation") #  # "line" adj
+Axis(side=1,at=seq(40, 160, by = 10))
+Axis(side=2, labels=FALSE)
+
+# Create data for the area to shade +-3SD
+cord.1x <-  c(80,seq(80,120,0.01),120) 
+cord.1y <-  c(0,dnorm(seq(80,120,0.01)),0) 
+# Make a curve
+#curve(dnorm(x,100,15), xlim=c(40,160), main='Standard Normal',lwd=2) 
+# Add the shaded area.
+polygon(cord.1x,cord.1y,col='grey60')
+#dev.off()
+
+
+
+
+dat <-  data.frame(x, y=dnorm(x,100,15))
+ggplot(data = dat, mapping = aes(x = x, y = y)) +
+  geom_line()+
+  geom_area(mapping = aes(x = ifelse(x>100 & x< 115 , x, 0)), fill = "darkgrey") +
+  scale_x_continuous(limits=c(40,160), breaks = c(100,115))+
+  scale_y_continuous(labels = NULL)+
+  ylab("QI distribution")
+  
+
+x <- seq(0,1,0.01)
+dat <- data.frame(x,y=2*x)
+ggplot(data=dat, mapping = aes(x=x,y=y))+
+  geom_line()+
+  geom_area(mapping = aes(x = ifelse(x>0.2 & x< 0.6 , x, 0)), fill = "darkgrey")+
+  scale_x_continuous(limits=c(0,2), breaks = c(0.2,0.6))+
+  scale_y_continuous(labels = NULL, limits=c(0,2))
+  
+
+
